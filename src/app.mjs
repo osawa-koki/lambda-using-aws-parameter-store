a9b7d0f3be88a4be49e410aca2e9793219519295
osawa-koki <koki@osawa.dev>
2024-01-08 22:28:00 +0900

    TypeScript化。

--- src/app.mjs
+++
@@ -1,8 +0,0 @@
-export const lambdaHandler = async (event, context) => {
-  return {
-    'statusCode': 200,
-    'body': JSON.stringify({
-      message: 'hello world!!!',
-    })
-  }
-}
--- src/tests/unit/test-handler.ts
+++ src/tests/unit/test-handler.ts
@@ -1,11 +1,18 @@
 'use strict'
 
 import { expect } from 'chai'
-import { lambdaHandler } from '../../app.mjs'
+import { lambdaHandler } from '../../app'
+import { APIGatewayProxyEvent, Context } from 'aws-lambda'
+
+const apiGatewayProxyEvent: APIGatewayProxyEvent = JSON.parse('{ "body": "{}" }')
+const context: Context = JSON.parse('{}')
+const callback = () => {}
 
 describe('Tests index', function () {
   it('verifies successful response', async () => {
-    const result = await lambdaHandler()
+    const result = await lambdaHandler(apiGatewayProxyEvent, context, callback)
+
+    if (result == null) throw new Error("result is null")
 
     expect(result).to.be.an('object')
     expect(result.statusCode).to.equal(200)
